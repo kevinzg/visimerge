d8c1957648ec8edf260c0dedbe7abd4b5fd23afc
Kevin Zúñiga <kevin.zun@gmail.com>
2018-03-30 17:08:17 -0500

    Add test for kernel visimergesort

---
+++ tests/test_kernel_visimergesort.cu
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include "visimerge/kernel_visimergesort.cuh"
+
+using namespace vmgpu;
+
+
+template<typename T>
+std::vector<segment<T>> readfile(const std::string &filename)
+{
+    typedef segment<T> segmentT;
+
+    std::ifstream file(filename.c_str());
+    std::string line;
+
+    std::vector<segmentT> vec;
+
+    while (std::getline(file, line))
+    {
+        std::stringstream ss(line);
+
+        segmentT seg;
+
+        ss >> seg.a.x;
+        ss.get();
+        ss >> seg.a.y;
+
+        ss >> seg.b.x;
+        ss.get();
+        ss >> seg.b.y;
+
+        vec.push_back(seg);
+    }
+
+    return vec;
+}
+
+
+template<typename T, typename Os>
+void print_viewrays(const std::vector<viewray<T>> &vec, Os &out)
+{
+    for (size_t i = 0; i < vec.size(); ++i)
+    {
+        out << vec[i].t << " "
+            << vec[i].v.x << " "
+            << vec[i].v.y << " "
+            << vec[i].r << " "
+            << vec[i].l << std::endl;
+    }
+}
+
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
+        std::cerr << argv[0] << ": missing file operand" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    auto vec = readfile<double>(argv[1]);
+
+    if (1u << mgpu::find_log2(vec.size(), true) != vec.size())
+    {
+        std::cerr << "current visimergesort only works for segment sets with 2^k segments" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::vector<viewray<double>> vis(vec.size() * 2);
+
+    mgpu::standard_context_t context(false);
+
+    kernel_visimergesort(vec.data(), vec.size(), vis.data(), context);
+
+    print_viewrays(vis, std::cout);
+
+    return 0;
+}
