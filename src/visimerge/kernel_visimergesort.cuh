9941de68b94650b7d149af364bbbaf314fceea8e
Kevin Zúñiga <kevin.zun@gmail.com>
2018-04-03 10:19:18 -0500

    Disable shared memory for cta_visimergesort kernel

--- src/visimerge/kernel_visimergesort.cuh
+++ src/visimerge/kernel_visimergesort.cuh
@@ -66,13 +66,15 @@ void kernel_visimergesort(segment<T> *segs, const int seg_count, viewray<T> *out
     mgpu::mem_t<viewrayT> dev_viewrays(vr_count, context);
     init_viewrays(dev_segments.data(), seg_count, dev_viewrays.data(), context, profile);
 
+    mgpu::mem_t<viewrayT> dev_buffer(vr_count, context);
+
     typedef mgpu::launch_params_t<16, 16> launch_t;
 
     const int nv = launch_t::nv(context);
     const int num_ctas = mgpu::div_up(seg_count, nv);
     const int num_passes = mgpu::find_log2(num_ctas, true);
 
-    auto k = [] MGPU_DEVICE (const int tid, const int cta, viewray<T> *input, const int seg_count)
+    auto k = [] MGPU_DEVICE (const int tid, const int cta, viewray<T> *input, const int seg_count, viewray<T> *buffer)
     {
         typedef typename launch_t::sm_ptx params_t;
         enum { nt = params_t::nt, vt = params_t::vt, nv = nt * vt };
@@ -81,31 +83,34 @@ void kernel_visimergesort(segment<T> *segs, const int seg_count, viewray<T> *out
         const mgpu::range_t cta_seg_range { nv * cta, min(static_cast<int>(seg_count), nv * (cta + 1)) };
         const mgpu::range_t cta_vr_range { cta_seg_range.begin * 2, cta_seg_range.end * 2 };
 
-        const int max_cta_vr_count = 2 * nv;
+        // const int max_cta_vr_count = 2 * nv;
 
-        __shared__ viewrayT shared_input[max_cta_vr_count];
-        __shared__ viewrayT shared_buffer[max_cta_vr_count];
+        // __shared__ viewrayT shared_input[max_cta_vr_count];
+        // __shared__ viewrayT shared_buffer[max_cta_vr_count];
 
-        __syncthreads();
+        // __syncthreads();
 
         // Shared memory
         mgpu::range_t thr_seg_range { vt * tid, vt * (tid + 1) };
         mgpu::range_t thr_vr_range { thr_seg_range.begin * 2, thr_seg_range.end * 2 };
 
-        for (int i = thr_vr_range.begin; i < thr_vr_range.end; ++i)
-            shared_input[i] = input[cta_vr_range.begin + i];
+        // for (int i = thr_vr_range.begin; i < thr_vr_range.end; ++i)
+        //     shared_input[i] = input[cta_vr_range.begin + i];
 
-        __syncthreads();
+        // __syncthreads();
+
+        viewrayT *shared_input = input + cta_vr_range.begin;
+        viewrayT *shared_buffer = buffer + cta_vr_range.begin;
 
         viewrayT *shared_output = cta_visimergesort<launch_t>(shared_input, shared_buffer, thr_seg_range, thr_vr_range, tid);
 
-        for (int i = thr_vr_range.begin; i < thr_vr_range.end; ++i)
-            input[cta_vr_range.begin + i] = shared_output[i];
+        // for (int i = thr_vr_range.begin; i < thr_vr_range.end; ++i)
+        //     input[cta_vr_range.begin + i] = shared_output[i];
     };
 
     if (profile) context.timer_begin();
 
-    mgpu::cta_launch<launch_t>(k, num_ctas, context, dev_viewrays.data(), seg_count);
+    mgpu::cta_launch<launch_t>(k, num_ctas, context, dev_viewrays.data(), seg_count, dev_buffer.data());
     context.synchronize();
 
     if (profile)
@@ -115,8 +120,6 @@ void kernel_visimergesort(segment<T> *segs, const int seg_count, viewray<T> *out
                   << num_ctas << " blocks of " << nv << " segments (" << vr_count * sizeof(viewrayT) / double(1 << 30) / s
                   << " GiB/s)" << std::endl;
     }
-
-    mgpu::mem_t<viewrayT> dev_buffer(vr_count, context);
 
     viewrayT *input_ptr = dev_viewrays.data();
     viewrayT *buffer_ptr = dev_buffer.data();
