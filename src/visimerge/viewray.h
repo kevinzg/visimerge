260e2a585f0f624b798598c533201520580134cc
Kevin Zúñiga <kevin.zun@gmail.com>
2018-04-30 12:00:26 -0500

    Add vray_array struct

--- src/visimerge/viewray.h
+++ src/visimerge/viewray.h
@@ -4,6 +4,10 @@
 #include <cmath>
 #include <moderngpu/meta.hxx>
 
+#ifdef __CUDACC__
+#include <moderngpu/context.hxx>
+#endif // __CUDACC__
+
 using std::cos;
 using std::sin;
 
@@ -33,6 +37,43 @@ struct viewray
     MGPU_HOST_DEVICE T vy() const { return sin(t); }
 #endif // __CUDACC__
 };
+
+
+#ifdef __CUDACC__
+template<typename T>
+struct MGPU_ALIGN(16) vray_array
+{
+    T *t{nullptr};
+    T *l{nullptr};
+    T *r{nullptr};
+
+    static vray_array create(size_t vr_count, mgpu::context_t &context,
+                             mgpu::memory_space_t space = mgpu::memory_space_device)
+    {
+        vray_array vr;
+        vr.t = static_cast<T*>(context.alloc(sizeof(T) * vr_count, space));
+        vr.l = static_cast<T*>(context.alloc(sizeof(T) * vr_count, space));
+        vr.r = static_cast<T*>(context.alloc(sizeof(T) * vr_count, space));
+
+        return vr;
+    }
+
+    static void destroy(vray_array &vr, mgpu::context_t &context,
+                        mgpu::memory_space_t space = mgpu::memory_space_device)
+    {
+        if (vr.t) context.free(vr.t, space);
+        if (vr.l) context.free(vr.l, space);
+        if (vr.r) context.free(vr.r, space);
+
+        vr.t = vr.l = vr.r = nullptr;
+    }
+
+    MGPU_HOST_DEVICE vray_array operator+(const int d) const
+    {
+        return vray_array { this->t + d, this->l + d, this->r + d };
+    }
+};
+#endif // __CUDACC__
 
 
 } // namespace vmgpu
